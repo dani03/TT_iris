86e53592d86c98011fed9907f8cc7c0dd7b5f083
daniel <lesamessipa@gmail.com>
2024-12-12 02:34:39 +0100

    add:authentification

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+
+    <title> {{ $title ?? 'page' }} - Mini blog</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+    @endif
+</head>
+
+<body class="font-sans antialiased dark:bg-black">
+    @yield('content')
+
+</body>
+
+</html>
